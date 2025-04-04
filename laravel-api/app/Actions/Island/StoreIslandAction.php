13fa7e3596f71517cc38b33b694617a7575bca27
ikreyms <ikreyms@gmail.com>
2025-04-04 07:12:58 +0500

    decoded hashids before creating island

--- laravel-api/app/Actions/Island/StoreIslandAction.php
+++ laravel-api/app/Actions/Island/StoreIslandAction.php
@@ -2,18 +2,23 @@
 
 namespace App\Actions\Island;
 
+use App\Models\Atoll;
 use App\Models\Island;
+use App\Services\Encoder;
 
 class StoreIslandAction
 {
     public static function run(array $data)
     {
+        $atollId = Encoder::decodeHashid($data['atoll_id']);
+        $islandCategoryId = Encoder::decodeHashid($data['island_category_id']);
+        
         $island = Island::create([
             'f_code' => $data['f_code'],
-            'atoll_id' => $data['atoll_id'],
+            'atoll_id' => $atollId,
             'name' => $data['name'],
             'area_sqm' => $data['area_sqm'],
-            'island_category_id' => $data['island_category'],
+            'island_category_id' => $islandCategoryId,
         ]);
 
         return $island;
