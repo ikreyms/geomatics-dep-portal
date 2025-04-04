efa99fe745676ed5e04c91d265aa6e08c005d61b
ikreyms <ikreyms@gmail.com>
2025-04-04 07:14:40 +0500

    added encodeHashid and decodeHashid

--- laravel-api/app/Services/Encoder.php
+++ laravel-api/app/Services/Encoder.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use Hashids\Hashids;
+use InvalidArgumentException;
 
 class Encoder {
 
@@ -19,5 +20,38 @@ class Encoder {
         }
 
         return static::$encoder;
+    }
+
+    /**
+     * Encode the model's ID (or any other field) using Hashids.
+     * 
+     * This method takes an ID and encodes it using the Hashids library
+     * to create a short, URL-safe hashid.
+     *
+     * @param  int $id
+     * @return string
+     */
+    public static function encodeHashid(int $id): string
+    {
+        return Encoder::getInstance()->encode($id);
+    }
+
+    /**
+     * Decode the hashid to retrieve the original ID.
+     * 
+     * This method decodes a given hashid and returns the corresponding
+     * model's ID. If the hashid is invalid, an exception is thrown.
+     *
+     * @param  string $hashid
+     * @return int|null
+     * @throws InvalidArgumentException if the hashid is invalid
+     */
+    public static function decodeHashid(string $hashid): ?int
+    {
+        $decoded = Encoder::getInstance()->decode($hashid);
+        if (!$decoded) {
+            throw new InvalidArgumentException('Invalid Hashid');
+        }
+        return $decoded[0];
     }
 }
