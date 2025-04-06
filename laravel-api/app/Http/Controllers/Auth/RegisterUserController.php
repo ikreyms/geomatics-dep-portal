74f51693b873a2a26366a1e8d7a165f9f3320603
ikreyms <ikreyms@gmail.com>
2025-04-06 13:40:32 +0500

    added registerSurveyor method to registerusercontroller

--- laravel-api/app/Http/Controllers/Auth/RegisterUserController.php
+++ laravel-api/app/Http/Controllers/Auth/RegisterUserController.php
@@ -2,25 +2,47 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Actions\Auth\RegisterStaffAction;
 use Exception;
 use App\Models\User;
-use App\Models\StaffProfile;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\RegisterStaffRequest;
-use App\Http\Resources\StaffProfileResource;
 use App\Http\Resources\UserResource;
+use App\Actions\Auth\RegisterStaffAction;
+use App\Actions\Auth\RegisterSurveyorAction;
+use App\Http\Resources\StaffProfileResource;
+use App\Http\Resources\SurveyorProfileResource;
+use App\Http\Requests\Auth\RegisterStaffRequest;
+use App\Http\Requests\Auth\RegisterSurveyorRequest;
 
 class RegisterUserController extends Controller
 {
     public function registerStaff(RegisterStaffRequest $request)
     {
-        $data = $request->validated();
-        [$user, $profile] = RegisterStaffAction::run($data);
-        return response()->json([
-            'user' => UserResource::make($user),
-            'profile' => StaffProfileResource::make($profile),
-        ]);
+        try {
+            $data = $request->validated();
+            [$user, $profile] = RegisterStaffAction::run($data);
+            return response()->json([
+                'user' => UserResource::make($user),
+                'profile' => StaffProfileResource::make($profile),
+            ]);
+        } catch (Exception $e) {
+            $this->logError($e, "Staff registration failed", $data ?? null);
+            return $this->somethingWentWrong();
+        }
+    }
+
+    public function registerSurveyor(RegisterSurveyorRequest $request)
+    {
+        try {
+            $data = $request->validated();
+            [$user, $profile] = RegisterSurveyorAction::run($data);
+            return response()->json([
+                'user' => UserResource::make($user),
+                'profile' => SurveyorProfileResource::make($profile),
+            ]);
+        } catch (Exception $e) {
+            $this->logError($e, "Surveyor registration failed", $data ?? null);
+            return $this->somethingWentWrong();
+        }
     }
 }
