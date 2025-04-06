5b2ef75a101fde16fe4c5d4e1aacb71d935b6639
ikreyms <ikreyms@gmail.com>
2025-04-06 13:32:44 +0500

    created surveyorprofiles table

---
+++ laravel-api/database/migrations/2025_04_06_071239_create_surveyor_profiles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('surveyor_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string(config('hashid.field'))->nullable();
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->string('nid');
+            $table->string('surveyor_reg_no');
+            $table->string('contact_no');
+
+            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('updated_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('deleted_by')->nullable()->constrained('users')->nullOnDelete();
+
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->index(config('hashid.field'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('surveyor_profiles');
+    }
+};
