163516a1aa871cb3dcaa490849740af470d3d261
ikreyms <ikreyms@gmail.com>
2025-04-07 09:51:36 +0500

    factory and seeder for Client

--- laravel-api/database/factories/ClientFactory.php
+++ laravel-api/database/factories/ClientFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Client;
+use App\Services\IdEncoderService;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,16 @@ class ClientFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->company(),
         ];
     }
+
+    public function configure(): static
+    {
+        return $this->afterCreating(function (Client $client) {
+            $client->hashid = IdEncoderService::encodeHashid($client->id);
+            $client->save();
+        });
+    }
+
 }
